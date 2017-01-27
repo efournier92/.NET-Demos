ebd13875716e60d4ff9db663d64f68814eb45bf2
Misigno <efournier92@gmail.com>
2017-01-26 19:30:05 -0500

    add dynamic page title example

---
+++ 01_SimpleSiteDemo/Index.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Super Simple C# Webpage!
+        </div>
+        <p>
+            <asp:Label ID="sampleLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ 01_SimpleSiteDemo/Index.aspx.cs
@@ -0,0 +1,36 @@
+﻿// using is a keyword
+// system is a namespace
+// using System includes existing code in project
+using System;
+
+// class isa required container for creating code samples
+public partial class Index : System.Web.UI.Page
+{
+    // runs when the page loads
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // allows for page title to be controlled dynamically
+        Page.Title = "Simple Site In C#";
+
+        var height = 65.00;
+        // binds var height to div sampleLabel
+        // $ allows for string interpolation
+        sampleLabel.Text = $"Ringo's height is {height}";
+        height = 55.5;
+        // appends new text to existing string
+        sampleLabel.Text += $"<br>George's height is {height}";
+
+        // {var:C} interpolates variable as currency
+        // {var:C} will round to nearest hundredth
+        const double dollars = 25.1222;
+        sampleLabel.Text += $"<br>{dollars} formatted a currency is {dollars:C}";
+
+        // {DateTime.Now:d} gets & shows a date
+        sampleLabel.Text += $"<br>Today's date is {DateTime.Now:d}";
+
+        var positiveBalance = true;
+        sampleLabel.Text += $"<br>Do I have a pisitive financial balance on {DateTime.Now:d}? (<b>{positiveBalance})</b>";
+
+
+    }
+}
