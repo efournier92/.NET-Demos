0c932d22610941e19d994d4bc3fc42b8f7f71c53
Misigno <efournier92@gmail.com>
2017-01-27 19:20:42 -0500

    start while loop syntax

--- 03_WhileLoops/Index.aspx.cs
+++ 03_WhileLoops/Index.aspx.cs
@@ -11,4 +11,16 @@ public partial class Index : System.Web.UI.Page
     {
 
     }
+
+    protected void sampleButton_Click(object sender, EventArgs e)
+    {
+        int numTimes = 5; // Represents how many times the loop in run
+        int counter = 1;
+
+        while (counter <= numTimes)
+        {
+            
+        }
+
+    }
 }
