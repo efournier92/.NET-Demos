a74656668640790f36be322a03a5f39afc198c2d
Misigno <efournier92@gmail.com>
2017-01-27 19:20:09 -0500

    fix persistent numbers bug

--- 02_NumLoopSiteDemo/Index.aspx.cs
+++ 02_NumLoopSiteDemo/Index.aspx.cs
@@ -14,6 +14,7 @@ public partial class Index : System.Web.UI.Page
     
     protected void numInput_TextChanged(object sender, EventArgs e)
     {
+        numOutput.Text = "";
         var n = Convert.ToInt32(numInput.Text);
         var i = 0;
         for ( ; i <= n; i++)
