f9b42ab89dc4d9405dd9c2193722fc8ac9bd953c
Misigno <efournier92@gmail.com>
2017-02-04 12:49:01 -0500

    finish TernaryOperator project

--- 07_TernaryOperator/Index.aspx
+++ 07_TernaryOperator/Index.aspx
@@ -8,9 +8,11 @@
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
-    
-    </div>
+        <asp:TextBox ID="ternaryTextBox01" runat="server"></asp:TextBox><br>
+        <asp:TextBox ID="ternaryTextBox02" runat="server"></asp:TextBox><br>
+        <asp:Button ID="ternaryButton" runat="server" Text="Find Sum" OnClick="ternaryButton_Click" /><br>
+        <asp:Label ID="ternaryLabel" runat="server" Text=""></asp:Label>
     </form>
 </body>
 </html>
+
--- 07_TernaryOperator/Index.aspx.cs
+++ 07_TernaryOperator/Index.aspx.cs
@@ -11,4 +11,17 @@ public partial class Index : System.Web.UI.Page
     {
 
     }
+
+    protected void ternaryButton_Click(object sender, EventArgs e)
+    {
+        double xOut, yOut; // we'll try to set these variables using TryParse
+        // combines ternary operator with TryParse
+        // if conversion is successful, xOut is saved to x
+        // if conversion fails, xOut gets the value 0
+        double x = double.TryParse(ternaryTextBox01.Text, out xOut) ? xOut : 0;
+
+        // same logic applys to y
+        double y = double.TryParse(ternaryTextBox02.Text, out yOut) ? yOut : 0;
+        ternaryLabel.Text = "Your sum is " + (x + y);
+    }
 }
