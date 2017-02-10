8931db01ad25bd3d9a2c2ba7e43d71c4a752ba39
Misigno <efournier92@gmail.com>
2017-02-09 22:09:44 -0500

    start PassingArraysToMethods

---
+++ 15_PassingArraysToMethods/Index.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Index : System.Web.UI.Page
+{
+    private static double Sum(double[] values)
+    {
+        // begin sum with 0
+        double sum = 0;
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
