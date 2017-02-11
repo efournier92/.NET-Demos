c07868356780079d35fdd6ce57e54332b31fda22
Misigno <efournier92@gmail.com>
2017-02-11 18:02:33 -0500

    create sum loop logic

--- 19_MultidimensionalArrays/Index.aspx.cs
+++ 19_MultidimensionalArrays/Index.aspx.cs
@@ -14,10 +14,16 @@ public partial class Index : System.Web.UI.Page
 
     protected void Button_Click(object sender, EventArgs e)
     {
-        double[,] arr = new double[2, 2]; // Deckares and sets 2x2 array (has 4 cells)
+        double[,] arr = new double[2, 2]; // Declares and sets 2x2 array (has 4 cells)
         arr[0, 0] = 45;
         arr[0, 1] = 34;
         arr[1, 0] = 23;
         arr[1, 0] = 78;
+        double sum = 0; // Begin with value of 0, so sum isn't changed
+        foreach (double d in arr)
+        {
+            sum += d; // Add entries together
+            Label.Text = "Sum is " + sum;
+        }
     }
 }
