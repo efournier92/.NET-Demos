98984b1a43d9cf6166788b915e300b745b04b20e
Misigno <efournier92@gmail.com>
2017-02-10 20:02:06 -0500

    reinit 18_OutKeywords

---
+++ 18_OutKeyword/Index.aspx.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Emit;
+
+public partial class Index : System.Web.UI.Page
+{
+    // out keyword indicates average value will be set inside Summarize
+    // average is also available to other methods
+
+}
