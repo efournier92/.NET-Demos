0896812ea017ffa1b0f8aadb3639ae5a00a3fff0
Misigno <efournier92@gmail.com>
2017-02-14 18:36:25 -0500

    add BankAccount class constructor

--- 21_CreatingClasses/Index.aspx.cs
+++ 21_CreatingClasses/Index.aspx.cs
@@ -15,6 +15,11 @@ public partial class Index : System.Web.UI.Page
         {
             balance = bal;
         }
+
+        public string GetBalance()
+        {
+            return "Balance:"
+        }
     }
 
 
