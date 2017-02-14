92ad34504f6ca245831ef503d3244230dcb36092
Misigno <efournier92@gmail.com>
2017-02-14 18:36:09 -0500

    add CreatingClasses project

---
+++ 21_CreatingClasses/Index.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Index : System.Web.UI.Page
+{
+    class BankAccount
+    {
+        private decimal balance;// instance variable, reachable only within this class
+
+        public BankAccount(decimal bal)
+        {
+            balance = bal;
+        }
+    }
+
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
