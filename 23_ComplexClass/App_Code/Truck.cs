7c2a326b259de0c04873b39f94ba6f15b74228d0
Misigno <efournier92@gmail.com>
2017-02-16 18:34:53 -0500

    finish Truck class file

---
+++ 23_ComplexClass/App_Code/Truck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Truck Class
+/// </summary>
+
+public class Truck
+{
+    private string make;
+    private static int count;
+    public Truck(string name)
+    {
+        make = name;
+        count++;
+    }
+    public static int TruckCount
+    {
+        get
+        {
+            return count;
+        }
+    }
+}
--- 23_ComplexClass/Index.aspx.cs
+++ 23_ComplexClass/Index.aspx.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class Index : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        var tr = new Truck(TextBox.Text);
+        Label.Text = Truck.TruckCount;
     }
 }
