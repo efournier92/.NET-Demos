eca4871c3e0ec42e9a46cba3600cf4c90b206451
Misigno <efournier92@gmail.com>
2017-02-16 18:35:47 -0500

    create Vehicle base class

---
+++ 24_ClassInheritance/App_Code/Vehicles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+/// <summary>
+/// Summary description for Vehicles
+/// </summary>
+public class Vehicle
+{
+    // instance variables
+    private string make;
+    private string model;
+
+    protected Vehicle(string mk, string mod)
+    {
+        make = mk;
+        model = mod;
+    }
+
+    // get vehicle make
+    protected string GetMake()
+    {
+        return make;
+    }
+}
+
