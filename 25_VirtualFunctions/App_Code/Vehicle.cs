3037e6e8d99908e132bf4d12371a3f27b6c1c768
Misigno <efournier92@gmail.com>
2017-02-17 08:07:52 -0500

    add Sedan class, derived from Vehicle

---
+++ 25_VirtualFunctions/App_Code/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System.CodeDom;
+
+/// <summary>
+/// Summary description for Vehicle
+/// </summary>
+public class Vehicle
+{
+    private string make;
+    public Vehicle(string mk)
+    {
+        make = mk;
+    }
+
+    public virtual string ShowInfo()
+    {
+        return "Make: " + make;
+    }
+}
+
+public class Sedan : Vehicle
+{
+    private int numberOfDoors;
+
+    public Sedan(string mk, int doors) : base(mk)
+    {
+        numberOfDoors = doors;
+    }
+
+    public override string ShowInfo()
+    {
+        return base.ShowInfo() + " Number of Doors: " + numberOfDoors;
+    }
+}
