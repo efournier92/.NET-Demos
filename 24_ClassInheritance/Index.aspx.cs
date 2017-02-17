2db8af8b163d91a5dd2ba71d3cd621076fc9a300
Misigno <efournier92@gmail.com>
2017-02-17 08:06:21 -0500

    add sedan & truck UI functionality

--- 24_ClassInheritance/Index.aspx.cs
+++ 24_ClassInheritance/Index.aspx.cs
@@ -9,6 +9,11 @@ public partial class Index : System.Web.UI.Page
 {
     protected void GetMakeButton_Click(object sender, EventArgs e)
     {
-
+        var sedanMakeModel = SedanTextBox.Text.Split(new char[] {','});
+        var truckMakeModel = TruckTextBox.Text.Split(new char[] {','});
+        var trk = new Truck(truckMakeModel[0], truckMakeModel[1]);
+        var sed = new Sedan(sedanMakeModel[0], sedanMakeModel[1]);
+        SedanTextBox.Text = "Sedan: " + sed.GetSedanMake();
+        TruckTextBox.Text = "Truck: " + trk.GetTruckMake();
     }
 }
