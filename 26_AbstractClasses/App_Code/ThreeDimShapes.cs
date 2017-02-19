3331f24574b275dbe7bd6dc8c9de4bdf80110618
Misigno <efournier92@gmail.com>
2017-02-18 20:48:53 -0500

    add ThreeDimShapes abstact class file

---
+++ 26_AbstractClasses/App_Code/ThreeDimShapes.cs
@@ -0,0 +1,11 @@
+﻿using static System.Math;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Shapes
+/// </summary>
+public abstract class ThreeDimShapes
+{
+    public abstract double GetVolume();
+}
