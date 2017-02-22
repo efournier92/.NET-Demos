42dde855564229a057089dda6e90b6ffc0606e6f
Misigno <efournier92@gmail.com>
2017-02-21 19:30:48 -0500

    start Product class file

---
+++ 29_Polymorphism/App_Code/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Products
+{
+    private decimal price;
+    public Products(decimal pri)
+    {
+        price = pri;
+    }
+
+    public virtual string GetDesc()
+    {
+        return "Price: " + price;
+    }
+}
