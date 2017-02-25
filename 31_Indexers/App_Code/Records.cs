7fbdfb5450173dfedefc853e12e5507921cac7a6
Misigno <efournier92@gmail.com>
2017-02-24 19:11:53 -0500

    start Records class file

---
+++ 31_Indexers/App_Code/Records.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Records
+{
+    public double[] records = new double[] { 42, 9, -5, 7, 101, -57, 70};
+}
