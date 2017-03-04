f569407760ca59938a6f6aafe2001ebac0b53069
Misigno <efournier92@gmail.com>
2017-03-04 13:00:22 -0500

    make null check logic work for CS5

---
+++ 34_NullOperator/Index.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Activities.Statements;
+using System.Collections.Generic;
+
+
+public partial class Index : System.Web.UI.Page
+{
+    protected void SearchButton_Click(object sender, EventArgs e)
+    {
+        Label.Text = "";
+        var carList = new List<Car>();
+        carList.Add(new Car("Toyota,Corolla", null));
+        carList.Add(new Car(null, "Ringo Starr"));
+        carList.Add(new Car("Saab,9-5", "Billy Mitchell"));
+        carList.Add(null);
+        foreach (var c in carList)
+        {
+                if (c.MakeModel != null)
+                    Label.Text += "<br>Make: " + c.MakeModel.Split(new char[] {','})[0];
+                Label.Text += "ERROR";
+        }
+    }
+}
