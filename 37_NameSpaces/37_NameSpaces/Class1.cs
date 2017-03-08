c8ebb80e9ba0a0d7814c61ebf3d58b5f83dbcbed
Misigno <efournier92@gmail.com>
2017-03-07 19:58:12 -0500

    create 37_NameSpaces project

---
+++ 37_NameSpaces/37_NameSpaces/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_NameSpaces
+{
+    public class Class1
+    {
+    }
+}
