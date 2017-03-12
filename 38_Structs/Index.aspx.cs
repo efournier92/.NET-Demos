0bf31a3eaa3c5d1e3143a54feb35e8d4ed250d67
Misigno <efournier92@gmail.com>
2017-03-12 12:25:47 -0400

    reference StructLibrary from website

--- 38_Structs/Index.aspx.cs
+++ 38_Structs/Index.aspx.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using StructLibrary;
 
 public partial class Index : System.Web.UI.Page
 {
 
+    protected void MakePoints_Click(object sender, EventArgs e)
+    {
+        Point[]
+    }
 }
