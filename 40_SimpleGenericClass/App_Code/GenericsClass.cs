cedbab8120d61e32a0909d3f1b9a93ed34bfb99e
Misigno <efournier92@gmail.com>
2017-03-15 20:38:03 -0400

    add UI elements

---
+++ 40_SimpleGenericClass/App_Code/GenericsClass.cs
@@ -0,0 +1,17 @@
+﻿public class GenericsClass<T>
+{
+    private T[] vals;
+    public GenericsClass(T[] input)
+    {
+        vals = input;
+    }
+
+    public string DisplayValues()
+    {
+        string str = null;
+        foreach (var T in vals)
+        {
+            str += "<br>Value = " + 
+        }
+    }
+}
---
+++ 40_SimpleGenericClass/Index.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Index : System.Web.UI.Page
+{
+    protected void DisplayValuesButton_Click(object sender, EventArgs e)
+    {
+
+    }
+}
