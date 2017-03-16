e33047e7fdcf546bb20c633c340a2e93cf5a0ba5
Misigno <efournier92@gmail.com>
2017-03-15 20:38:21 -0400

    add button_click logic

--- 40_SimpleGenericClass/Index.aspx.cs
+++ 40_SimpleGenericClass/Index.aspx.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class Index : System.Web.UI.Page
 {
     protected void DisplayValuesButton_Click(object sender, EventArgs e)
     {
+        GenericsClass<int> ints = new GenericsClass<int>(new int[] { 1, 2, 3, 4, 5});
+        Label.Text = ints.DisplayValues();
 
+        GenericsClass<double> dubs = new GenericsClass<double>(new double[] { 1, 2, 3, 4, 5});
+        Label.Text = dubs.DisplayValues();
+
+        GenericsClass<decimal> dec = new GenericsClass<decimal>(new decimal[] { 1, 2, 3, 4, 5 });
+        Label.Text = dec.DisplayValues();
     }
 }
