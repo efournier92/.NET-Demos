74d0f0e45b8881c2a24f7bae2c047622a202d625
Misigno <efournier92@gmail.com>
2017-03-20 18:43:52 -0400

    finish GenericMethod class logic

--- 41_GenericMethod/App_Code/GenericMethod.cs
+++ 41_GenericMethod/App_Code/GenericMethod.cs
@@ -7,6 +7,17 @@ public class GenericMethod
 {
     public static void Swap<T>(ref T x, ref T y)
     {
-        T temp = 
+        var temp = x;
+        x = y;
+        y = temp;
+    }
+
+    public static string Compare<T>(T x, T y) where T : IComparable
+    {
+        if (x.CompareTo(y) < 0)
+        {
+            return x + " < " + y;
+        }
+        return x + " > " + y;
     }
 }
