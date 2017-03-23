93a458a1854b65f94fdd61db740f69d21ef28a8b
Misigno <efournier92@gmail.com>
2017-03-23 18:35:10 -0400

    add Square & Rectangle classes

--- 42_GenericInterface/App_Code/GenericInterface.cs
+++ 42_GenericInterface/App_Code/GenericInterface.cs
@@ -27,13 +27,36 @@ public class Quad : IComparable<Quad>
 
     public virtual string Perimeter()
     {
-        return "The perimeter of " + name + " is " + 
+        return "The perimeter of " + name + " is " +
     }
 }
 
 public class Square : Quad
 {
+    private double sideLen;
 
+    public Square(string n, double s) : base(n)
+    {
+        sideLen = s;
+    }
+    public override string Perimeter()
+    {
+        return base.Perimeter() + 4 * sideLen;
+    }
+}
+public class Rectangle : Quad
+{
+    private double sideOne, sideTwo;
+
+    public Rectangle(string n, double s1, double s2) : base(n)
+    {
+        sideOne = s1;
+        sideTwo = s2;
+    }
+    public override string Perimeter()
+    {
+        return base.Perimeter() + (2 * sideOne + 2 * sideTwo);
+    }
 }
 
 /// <summary>
