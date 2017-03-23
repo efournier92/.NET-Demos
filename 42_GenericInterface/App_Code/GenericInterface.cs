92a39418c4e6daaa8e7bc5f211d2234b8768f5e5
Misigno <efournier92@gmail.com>
2017-03-23 18:34:49 -0400

    create Quad class

--- 42_GenericInterface/App_Code/GenericInterface.cs
+++ 42_GenericInterface/App_Code/GenericInterface.cs
@@ -1,7 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Web;
+
+public class Quad : IComparable<Quad>
+{
+    private string name;
+
+    public Quad(string na)
+    {
+        name = na;
+    }
+
+    public int CompareTo(Quad other)
+    {
+        if (this.name.CompareTo(other.name) < 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    public virtual string Perimeter()
+    {
+        return "The perimeter of " + name + " is " + 
+    }
+}
+
+public class Square : Quad
+{
+
+}
 
 /// <summary>
 /// Summary description for GenericInterface
