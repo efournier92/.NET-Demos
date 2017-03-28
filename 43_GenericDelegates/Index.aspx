9a18256afacf827020312e584f40e6c867599386
Misigno <efournier92@gmail.com>
2017-03-28 18:55:05 -0400

    add UI elements

---
+++ 43_GenericDelegates/Index.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Generic Delegates</title>
+</head>
+<body>
+    <asp:Button ID="SummarizeButton" runat="server" Text="Button" OnClick="Button_Click" /><br />
+    <asp:Label ID="Label" runat="server" Text="Label"></asp:Label>
+</body>
+</html>
---
+++ 43_GenericDelegates/Index.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public delegate void Summarize<T>(T x, T y);
+public partial class Index : System.Web.UI.Page
+{
+
+    protected void Button_Click(object sender, EventArgs e)
+    {
+        Summarize<double> s = FindSum;
+        s += FindRatio;
+        s += FindProduct;
+        s(4, 5);
+    }
+
+    public void FindSum(double x, double y)
+    {
+        Label.Text = "<br>" + x + " + " + y + " = " + (x + y);
+    }
+
+    public void FindRatio(double x, double y)
+    {
+        Label.Text = "<br>" + x + " / " + y + " = " + (x / y);
+    }
+    public void FindProduct(double x, double y)
+    {
+        Label.Text = "<br>" + x + " * " + y + " = " + (x * y);
+    }
+}
