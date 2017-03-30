24bbc8592097553aef57490c68aed9b29d27be4c
Misigno <efournier92@gmail.com>
2017-03-30 18:51:53 -0400

    refactor & finish 45_DelegatesAndLambdas

--- 45_DelegatesAndLambdas/Index.aspx
+++ 45_DelegatesAndLambdas/Index.aspx
@@ -4,11 +4,11 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Delegates & Lambdas</title>
 </head>
 <body>
-    <form id="form1" runat="server">
-        <asp:Button ID="ShowResultsButton" runat="server" Text="Show Results" OnClick="ShowResultsButton_Click" />
+    <form runat="server">
+        <asp:Button runat="server" Text="Show Results" OnClick="ShowResultsButton_Click" />
         <asp:Label ID="Label" runat="server"></asp:Label>
     </form>
 </body>
--- 45_DelegatesAndLambdas/Index.aspx.cs
+++ 45_DelegatesAndLambdas/Index.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public delegate bool Compare(double x, double y);
 public delegate double Multiply(double x, double y);
@@ -12,9 +7,9 @@ public partial class Index : System.Web.UI.Page
     protected void ShowResultsButton_Click(object sender, EventArgs e)
     {
         Label.Text = "";
-        double x = 10, y = 25;
+        const double x = 10, y = 25;
         Compare comp = (a, b) => (a == b);
-        Label.Text = "<br>" + x + " and " + y + " are equal is: " + comp(x, y).ToString().ToUpper();
+        Label.Text = "<br>" + x + " & " + y + " are equal? " + comp(x, y).ToString().ToUpper();
         Multiply mult = (a, b) => (a * b);
         Label.Text += "<br>" + x + " * " + y + " = " + mult(x, y);
         var dubsArray = new double[] { 1, 2, 3, 4, 5 };
