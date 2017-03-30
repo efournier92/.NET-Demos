27c67440c5b09f5f80afc18c72c218ef9c37518c
Misigno <efournier92@gmail.com>
2017-03-30 18:51:23 -0400

    finish Button_Click logic

--- 45_DelegatesAndLambdas/Index.aspx.cs
+++ 45_DelegatesAndLambdas/Index.aspx.cs
@@ -11,10 +11,17 @@ public partial class Index : System.Web.UI.Page
 {
     protected void ShowResultsButton_Click(object sender, EventArgs e)
     {
+        Label.Text = "";
         double x = 10, y = 25;
         Compare comp = (a, b) => (a == b);
-        Label.Text = x + " and " + y + " are equal is: " + comp(x, y).ToString().ToUpper();
+        Label.Text = "<br>" + x + " and " + y + " are equal is: " + comp(x, y).ToString().ToUpper();
         Multiply mult = (a, b) => (a * b);
-        Label.Text += x + " * " + y + " = " + mult(x, y);
+        Label.Text += "<br>" + x + " * " + y + " = " + mult(x, y);
+        var dubsArray = new double[] { 1, 2, 3, 4, 5 };
+        Action<double> showDouble = (a) => Label.Text += "<br>" + (a * a);
+        foreach (var d in dubsArray)
+        {
+            showDouble(d);
+        }
     }
 }
