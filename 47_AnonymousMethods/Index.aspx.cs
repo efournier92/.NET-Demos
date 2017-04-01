14f3688b9ea955075ac81e1b6299b780e154335c
Misigno <efournier92@gmail.com>
2017-04-01 12:15:37 -0400

    finish writing ShowResultsButton_Click

--- 47_AnonymousMethods/Index.aspx.cs
+++ 47_AnonymousMethods/Index.aspx.cs
@@ -6,17 +6,24 @@ public partial class Index : System.Web.UI.Page
 {
     private void ShowSquare(double x)
     {
-        Label.Text += "<br>" + (x*x);
+        Label.Text += "<br>" + (x * x);
     }
 
     protected void ShowResultsButton_Click(object sender, EventArgs e)
     {
-        var vals = new List<double>(new double[] {1, 2, 3, 4, 5, 6});
+        var vals = new List<double>(new double[] { 1, 2, 3, 4, 5, 6 });
         vals.ForEach(ShowSquare);
-        vals.ForEach(delegate(double x)
+
+        vals.ForEach(delegate (double x)
         {
             Label.Text += "<br>" + Math.Pow(x, 3);
         });
-
+        var td = new Thread(delegate ()
+        {
+            var arrs = new List<double>(new double[] {1, 4, 5, 11, 53, 42});
+            arrs.Sort();
+            arrs.ForEach(x => Label.Text += "<br>" + x);
+        });
+        td.Start();
     }
 }
