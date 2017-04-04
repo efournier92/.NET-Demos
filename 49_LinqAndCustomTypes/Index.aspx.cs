c6663f300a2f6d8e1d632e99c11b15146b46aa93
Misigno <efournier92@gmail.com>
2017-04-04 18:51:37 -0400

    write people sorting functionality

--- 49_LinqAndCustomTypes/Index.aspx.cs
+++ 49_LinqAndCustomTypes/Index.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public class Person
@@ -17,6 +18,12 @@ public partial class Index : System.Web.UI.Page
 {
     protected void ShowPeopleButton_Click(object sender, EventArgs e)
     {
-
+        var people = new Person[] { new Person("John", 1000000), new Person("Joan", 50000), new Person("Paul", 1000000), new Person("Ringo", 25000)};
+        IEnumerable<Person> peopleWithO =
+            people.Where(person => person.Name.EndsWith("n")).OrderByDescending(person => person.Salary);
+        foreach (var person in peopleWithO)
+        {
+            Label.Text += $"<br>{person.Name}        {person.Salary:C}";
+        }
     }
 }
