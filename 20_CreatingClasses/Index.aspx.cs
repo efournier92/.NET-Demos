63b50e79163d6c514f014c4c8fee2feda75b4bc2
Misigno <efournier92@gmail.com>
2017-04-10 18:57:05 -0400

    refacto 22_CreatingClasses

---
+++ 20_CreatingClasses/Index.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public partial class Index : System.Web.UI.Page
+{
+    private class BankAccount
+    {
+        private readonly decimal _balance;// instance variable, reachable only within this class
+
+        public BankAccount(decimal bal)
+        {
+            _balance = bal;
+        }
+
+        public string GetBalance()
+        {
+            return $"<b>Balance:</b> {_balance:C}";
+        }
+    }
+
+    protected void Button_Click(object sender, EventArgs e)
+    {
+        var ba = new BankAccount(5002);
+        Label.Text = ba.GetBalance();
+    }
+}
