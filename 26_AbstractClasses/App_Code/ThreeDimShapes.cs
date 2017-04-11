b2483f57d53151b1a0d4e43e425976223a5ff32d
Misigno <efournier92@gmail.com>
2017-04-11 18:34:59 -0400

    refactor 26_AbstractClasses

---
+++ 26_AbstractClasses/App_Code/ThreeDimShapes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Shapes
+/// </summary>
+public abstract class ThreeDimShape
+{
+    public abstract double GetVolume();
+}
+
+public class Sphere : ThreeDimShape
+{
+    private readonly double _radius;
+
+    public Sphere(double r)
+    {
+        _radius = r;
+    }
+
+    public override double GetVolume()
+    {
+        return (4.00) / (3.00) * Math.PI * Math.Pow(_radius, 3);
+    }
+}
+
+public class Cube : ThreeDimShape
+{
+    private readonly double _edge;
+
+    public Cube(double edgeLength)
+    {
+        _edge = edgeLength;
+    }
+
+    public override double GetVolume()
+    {
+        return Math.Pow(_edge, 3);
+    }
+}
