75fd15c14c00f2b71489637db643bd2aed7b7d8d
Misigno <efournier92@gmail.com>
2017-04-13 19:37:16 -0400

    refactor 38_Structs

---
+++ 38_Structs/Index.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Structs</title>
+</head>
+<body>
+    <form runat="server">
+        <asp:Button runat="server" Text="Make & Show Points" OnClick="MakePoints_Click" /><br />
+        <asp:Label ID="PointsLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ 38_Structs/Index.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using StructLibrary;
+
+public partial class Index : System.Web.UI.Page
+{
+
+    protected void MakePoints_Click(object sender, EventArgs e)
+    {
+        PointsLabel.Text = "";
+        var points = new Point[10];
+        for (var i = 0; i < points.Length; i++)
+        {
+            var x = new Random().Next(-10, 10);
+            Thread.Sleep(20);
+            var y = new Random().Next(-10, 10);
+            Thread.Sleep(20);
+            points[i] = new Point(x, y);
+
+        }
+        foreach (var p in points)
+        {
+            PointsLabel.Text += "(" + p.X + ", " + p.Y + ")<br>";
+        }
+    }
+}
