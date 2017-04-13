9247709d9dc03dc9b91c02b2c725298f3090f413
Misigno <efournier92@gmail.com>
2017-04-12 20:47:09 -0400

    refactor 32_Interfaces

---
+++ 30_Interfaces/Index.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public partial class Index : System.Web.UI.Page
+{
+    protected void PrintButton_Click(object sender, EventArgs e)
+    {
+        IPrintable per = new Person(NameBox.Text);
+        IPrintable book = new Document(QuoteBox.Text);
+        var pr = new List<IPrintable> { per, book };
+        pr.ForEach(x => Label.Text += x.Print());
+    }
+}
