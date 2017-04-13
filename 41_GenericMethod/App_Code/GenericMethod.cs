24bbc3b4cdd5ff07b9e827b5ea27e0c4510450bd
Misigno <efournier92@gmail.com>
2017-04-13 19:38:56 -0400

    refactor 41_GenericMethod

---
+++ 41_GenericMethod/App_Code/GenericMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for GenericMethod
+/// </summary>
+public static class GenericMethod
+{
+    public static void Swap<T>(ref T x, ref T y)
+    {
+        var temp = x;
+        x = y;
+        y = temp;
+    }
+
+    public static string Compare<T>(T x, T y) where T : IComparable
+    {
+        if (x.CompareTo(y) < 0)
+        {
+            return x + " < " + y;
+        }
+        return x + " > " + y;
+    }
+}
