f799ce334a535f03c016892132f72146fbd09d5b
Misigno <efournier92@gmail.com>
2017-04-12 20:48:30 -0400

    refactor 34_NullOperator

---
+++ 34_NullOperator/Index.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Null Operator</title>
+</head>
+<body>
+    <form runat="server">
+        <asp:Button runat="server" Text="Get Car Inventory" OnClick="SearchButton_Click" /><br />
+        <asp:Label ID="Label" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ 34_NullOperator/Index.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public partial class Index : System.Web.UI.Page
+{
+    protected void SearchButton_Click(object sender, EventArgs e)
+    {
+        Label.Text = "";
+        var carList = new List<Car>
+        {
+            new Car("Toyota,Corolla", null),
+            new Car(null, "Ringo Starr"),
+            new Car("Saab,9-5", "Billy Mitchell"),
+            null
+        };
+        foreach (var c in carList.Where(c => c != null))
+        {
+            if (c.MakeModel != null)
+            {
+                Label.Text += "<br>Make: " + c.MakeModel.Split(new char[] { ',' })[0];
+                if (c.MakeModel.Split(new char[] { ',' })[1] != null)
+                {
+                    Label.Text += "<br>Model: " + c.MakeModel.Split(new char[] { ',' })[1];
+                }
+                else
+                {
+                    Label.Text += "<br>Model: NULL";
+                }
+            }
+            else
+            {
+                Label.Text += "<br>Make: NULL";
+                Label.Text += "<br>Model: NULL";
+            }
+            if (c.PreviousOwner != null)
+            {
+                Label.Text += "<br>Previous Owner: " + c.PreviousOwner;
+            }
+            else
+            {
+                Label.Text += "<br>Previous Owner: NULL";
+            }
+            Label.Text += "<br>";
+        }
+    }
+}
