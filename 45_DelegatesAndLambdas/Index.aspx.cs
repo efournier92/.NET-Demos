a3f76ccfb92b7b3d1f9c5908fec5fcddb034ae50
Misigno <efournier92@gmail.com>
2017-04-14 08:07:47 -0400

    refactor 46_ExpressionBodiedLambdas

---
+++ 45_DelegatesAndLambdas/Index.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public delegate bool Compare(double x, double y);
+public delegate double Multiply(double x, double y);
+public partial class Index : System.Web.UI.Page
+{
+    protected void ShowResultsButton_Click(object sender, EventArgs e)
+    {
+        Label.Text = "";
+        const double x = 10, y = 25;
+        Compare comp = (a, b) => (a == b);
+        Label.Text = $"<br>{x} & {y} are equal? {comp(x, y).ToString().ToUpper()}";
+        Multiply mult = (a, b) => (a * b);
+        Label.Text += $"<br>{x} * {y} = {mult(x, y)}";
+        var dubsArray = new double[] { 1, 2, 3, 4, 5 };
+        Action<double> showDouble = (a) => Label.Text += $"<br> {a * a}";
+        foreach (var d in dubsArray)
+        {
+            showDouble(d);
+        }
+    }
+}
---
+++ 46_ExpressionBodiedLambdas/Index.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Expression Bodied Lambdas</title>
+</head>
+<body>
+    <form runat="server">
+        Enter Values: <asp:TextBox ID="TextBox" runat="server"></asp:TextBox><br />
+        <asp:Button runat="server" Text="Find Max" OnClick="FindMaxButton_Click"/><br/>
+        <asp:Label ID="Label" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
