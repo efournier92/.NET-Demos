4e85465e59845376147e59555d88b651c775212d
efournier92 <efournier92@gmail.com>
2017-05-23 19:09:12 -0400

    refactor 11_ArrayBasics

---
+++ 11_ArrayBasics/Index.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Array Basics</title>
+</head>
+<body>
+    <form runat="server">
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+        <asp:Button runat="server" Text="Summarize" OnClick="SummarizeButton_Click" /><br />
+        <asp:Label ID="Label" runat="server" Text=""></asp:Label>
+    </form>
+</body>
+</html>
---
+++ 11_ArrayBasics/Index.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public partial class Index : System.Web.UI.Page
+{
+    protected void SummarizeButton_Click(object sender, EventArgs e)
+    {
+        // create array that can store 2 entries
+        var values = new decimal[2];
+        // converts & saves value from box to values[0]
+        values[0] = decimal.Parse(TextBox1.Text);
+        // converts & saves value from box to valu es[1]
+        values[1] = decimal.Parse(TextBox2.Text);
+        // add array values together
+        var total = values[0] + values[1];
+        // display total value
+        Label.Text = $"Total Is {total}";
+        // return arithmetical average
+        Label.Text += $"<br>Average Is {total / values.Length}";
+    }
+}
