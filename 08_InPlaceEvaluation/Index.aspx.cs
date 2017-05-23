eed98f2d49ec8c165a1ccb652112c90d1eac8ed3
efournier92 <efournier92@gmail.com>
2017-05-23 19:07:29 -0400

    refactor 08_InPlaceEvaluation

--- 08_InPlaceEvaluation/Index.aspx.cs
+++ 08_InPlaceEvaluation/Index.aspx.cs
@@ -2,15 +2,14 @@
 
 public partial class Index : System.Web.UI.Page
 {
-    // Button_Click is code that runs when a page loads from server
     protected void Button_Click(object sender, EventArgs e)
     {
         double x = 25;
         Label.Text = $"{x} Single Increased Is {++x}";
-        Label.Text += "<br>" + x + " Double Increased Is " + (x += 2);
-        Label.Text += "<br>" + x + " Single Decreased Is " + (--x);
-        Label.Text += "<br>" + x + " Double Decreased Is " + (x -= 2);
-        Label.Text += "<br>Does x = 3? " + (x == 3);
-        Label.Text += "<br>Does x = 25? " + (x == 25);
+        Label.Text += $"<br> {x} Double Increased Is {x += 2}";
+        Label.Text += $"<br> {x} Single Decreased Is {--x}";
+        Label.Text += $"<br> {x} Double Decreased Is {x -= 2}";
+        Label.Text += $"<br>Does x = 3? {x == 3}";
+        Label.Text += $"<br>Does x = 25? {x == 25}";
     }
 }
