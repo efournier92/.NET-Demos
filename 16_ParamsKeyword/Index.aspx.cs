1eab482fa227ec9750061b4a2bc4818f893ced09
efournier92 <efournier92@gmail.com>
2017-05-25 18:53:14 -0400

    refactor 16_ParamsKeyword

---
+++ 16_ParamsKeyword/Index.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public partial class Index : System.Web.UI.Page
+{
+    // params keyword lets us pass a variable number of values into a method
+    // Product(1,2) or Product(-3,4,3,6)
+    private static double Product(params double[] arr)
+    {
+        return arr.Aggregate<double, double>(1, (current, d) => current*d);
+    }
+
+    protected void Button_Click(object sender, EventArgs e)
+    {
+        const double x = 5, y = 10, z = 15;
+        Label.Text = $"{x} * {y} = {Product(x, y)}";
+        Label.Text += $"<br> {x} * {y} * {z} = {Product(x, y, z)}";
+    }
+}
